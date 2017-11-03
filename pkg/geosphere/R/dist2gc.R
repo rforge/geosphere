b22a62abd33d2b834dcf5dcbb8de27dc9854a104
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-11-03 05:38:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geosphere@236 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geosphere/R/dist2gc.R
+++ pkg/geosphere/R/dist2gc.R
@@ -18,8 +18,8 @@ dist2gc <- function(p1, p2, p3, r=6378137, sign=FALSE) {
 	p3 <- p[,5:6]
 	r <- p[,7]
 	
-	tc <- bearing(p1, p2) * toRad
-	tcp <- bearing(p1, p3) * toRad
+	tc <- bearing(p1, p2, a=r, f=0) * toRad
+	tcp <- bearing(p1, p3, a=r, f=0) * toRad
     dp <- distCosine(p1, p3, r=1)
 	xtr <- (asin(sin(tcp-tc) * sin(dp)) * r)
 	xtr <- as.vector(xtr)
